61252ab2a764eb493514d93d9549351aa322409c
Reamah <ezareettah@gmail.com>
2023-06-15 21:08:11 +0300

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,3 @@
+# Auto detect text files and perform LF normalization
+* text=auto
+print("Привет, мир!\nОднажды в студёную зимнюю пору\nЯ из лесу вышел; был сильный мороз.\nГляжу, поднимается медленно в гору")
